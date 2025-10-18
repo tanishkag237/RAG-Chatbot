4815f7d3add539001203e7b029c2718001b6b308
tanishkag237 <tgoel029@gmail.com>
2025-10-18 12:05:27 +0530

    add streamlit ui

---
+++ chat_ui.py
@@ -0,0 +1,28 @@
+import streamlit as st
+from chatbot import getRAGchain
+
+st.title("RAG Chatbot ")
+
+@st.cache_resource
+def load_chain():
+    return getRAGchain()
+
+rag_chain = load_chain()
+
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+
+user_query = st.chat_input("Ask your question...")
+
+if user_query:
+    with st.spinner("Thinking..."):
+        result = rag_chain.invoke({"query": user_query})
+        answer = result["result"]
+
+    st.session_state.chat_history.append(("You", user_query))
+    st.session_state.chat_history.append(("Bot", answer))
+
+# display chat history
+for role, msg in st.session_state.chat_history:
+    with st.chat_message("user" if role == "You" else "assistant"):
+        st.markdown(msg)
--- chatbot.py
+++ chatbot.py
@@ -4,24 +4,34 @@ from transformers import pipeline
 from langchain_community.llms import HuggingFacePipeline
 from langchain.chains import RetrievalQA
 
-embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-vectorstore = Chroma(persist_directory="chroma_db", embedding_function=embedding_model)
 
-hf_pipeline = pipeline(
-    "text2text-generation",  
-    model="MBZUAI/LaMini-Flan-T5-783M",  # or flan-t5-small
-    max_new_tokens=256,
-    temperature=0.2
-)
+def getRAGchain():
+    embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    vectorstore = Chroma(persist_directory="chroma_db", embedding_function=embedding_model)
 
-llm = HuggingFacePipeline(pipeline=hf_pipeline)
-retriever = vectorstore.as_retriever(search_kwargs={"k": 3})
+    hf_pipeline = pipeline(
+        "text2text-generation",
+        model="MBZUAI/LaMini-Flan-T5-783M",  # or flan-t5-small
+        max_new_tokens=256,
+        temperature=0.2,
+    )
 
-rag_chain = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
+    llm = HuggingFacePipeline(pipeline=hf_pipeline)
 
-while True:
-    query = input("\n Ask something (or 'exit'): ")
-    if query.lower() == "exit":
-        break
-    result = rag_chain.invoke({"query": query})
-    print("\n Answer:", result["result"], "\n")
+    # converts vector store into a retriever
+    # finds the top k most similar documents to the query
+    retriever = vectorstore.as_retriever(search_kwargs={"k": 3}) 
+    rag_chain = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
+
+    return rag_chain        
+
+
+
+if __name__ == "__main__":
+    rag_chain = getRAGchain()
+    while True:
+        query = input("\nAsk something (or 'exit'): ")
+        if query.lower() == "exit":
+            break
+        result = rag_chain.invoke({"query": query})
+        print("\nAnswer:", result["result"], "\n")
